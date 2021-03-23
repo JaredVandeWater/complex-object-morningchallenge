ff0aed037c929b0e8ea123c54aaf847c032b2011
NiceJared <jaredvande@gmail.com>
2021-03-23 09:40:12 -0600

    asa

--- my-object.js
+++ my-object.js
@@ -1,15 +1,26 @@
 let gameboy = {
-    Type: 'advanced SP',
+    type: 'Gameboy Advanced SP',
     color: 'silver',
+    buttonColor: 'Gray',
     buttons: ['start', 'select', 'A', 'B', 'L', 'R', 'd-pad', 'back-light toggle'],
     displayType: 'LCD',
-
+    displaySizeInch: 2.9,
+    batteryLeftPercent: .64,
     currentGame: {
         title: 'The Legend of Zelda: Minish Cap',
-        coverColor: 'red',
+        coverBackgroundColor: 'red',
         saveFileName: 'Jared',
+        playerHearts: 17,
         progress: .83,
+        releaseDate: 2004
+    },
+    releaseDate: 2003,
+    charger: {
+        color: 'black',
+        pluggedIn: false,
+        cableLengthFeet: 2,
     }
+
 
 
 }
