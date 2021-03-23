ecdf862acfbe01a96fa508354ccb153d6367a96c
NiceJared <jaredvande@gmail.com>
2021-03-23 09:36:21 -0600

    first commit

---
+++ my-object.js
@@ -0,0 +1,15 @@
+let gameboy = {
+    Type: 'advanced SP',
+    color: 'silver',
+    buttons: ['start', 'select', 'A', 'B', 'L', 'R', 'd-pad', 'back-light toggle'],
+    displayType: 'LCD',
+
+    currentGame: {
+        title: 'The Legend of Zelda: Minish Cap',
+        coverColor: 'red',
+        saveFileName: 'Jared',
+        progress: .83,
+    }
+
+
+}
